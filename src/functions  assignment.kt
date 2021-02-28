799a951dff43fd72cd942fc59615ddacbfb571a8
brendagitau <brendagittau@gmail.com>
2021-02-28 14:08:32 +0300

    functions assignment

---
+++ src/functions  assignment.kt
@@ -0,0 +1,21 @@
+fun main(){
+    jina()
+    println(22%7)
+    println(1+2+3+4)
+    special()
+}
+fun jina(){
+    var name = "Brenda Gitau"
+    println(
+     "Hello $name"
+    )
+}
+fun modulus(a:Int, b:Int): Int{
+    return(a%b)
+}
+fun sum(num1:Int, num2:Int, num3:Int, num4:Int): Int{
+    return(num1+num2+num3+num4)
+}
+fun special(){
+    println("I am able to do anything I set my mind on.")
+}
